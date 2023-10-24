fc78e412f5670d90892d8d2f8beb0599d768aa0f
Tirito6626 <easycraft250@gmail.com>
2023-10-24 18:29:39 +0200

    added latency_api and latency_websocket

--- src/modules/websocket.js
+++ src/modules/websocket.js
@@ -2,7 +2,7 @@ const WebSocket = require("ws");
 const client = require("/etc/bashcord/config.json")
 const ws = new WebSocket('wss://gateway.discord.gg/?v=10&encoding=json');
 let heartbeatInterval;
-
+let delay
 let lastSeq;
 function Connect() {
 ws.on('open', () => {
@@ -12,29 +12,24 @@ ws.on('open', () => {
 });
 
 ws.on('message', message => {
-
+    startTime = new Date().getTime();
     const { d: data, t: event, op, s: seq } = JSON.parse(message);
-    
+    ws.ping('ping');
+    ws.on('pong', function(message) {
     lastSeq = seq;
-console.log(JSON.stringify({ d: data, t: event, op: op, s: seq}))
+    let endTime = new Date().getTime();
+    delay = endTime - startTime;
+    })
+console.log(JSON.stringify({ d: data, t: event, op: op, s: seq, p: delay }))
     switch (op) {
-
         case 10:
-
             heartbeatInterval = setInterval(() => {
-
                 ws.send(JSON.stringify({
-
                     op: 1,
-
                     d: lastSeq || null
-
                 }));
-
             }, data.heartbeat_interval);
-
             break;
-
     }
 });
 
